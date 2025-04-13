7798fd7ea063feb0fd9bebd7158cae1173b534be
marco-botte <marco.botte90@googlemail.com>
2025-04-13 17:10:46 +0200

    Add main package

---
+++ clean_input_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "go is    fun ",
+			expected: []string{"go", "is", "fun"},
+		},
+		{
+			input:    "     ",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("cleanInput(%q) = %v; want %v", c.input, actual, c.expected)
+		}
+	}
+}
