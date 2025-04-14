6f6441105f522d911287181a7f70ac1d9364e218
marco-botte <marco.botte90@googlemail.com>
2025-04-14 09:20:12 +0200

    Move commands into separate file

--- main.go
+++ main.go
@@ -7,30 +7,6 @@ import (
 	"strings"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-var commMap map[string]cliCommand
-
-func commandHelp() error {
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println("")
-	for _, command := range commMap {
-		fmt.Println(command.name, ": ", command.description)
-	}
-	return nil
-}
-
-func commandExit() error {
-	defer os.Exit(0)
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	return nil
-}
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	commMap = map[string]cliCommand{
