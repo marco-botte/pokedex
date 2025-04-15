5418037367f1aa559d4d41cd9f59d877856df356
marco-botte <marco.botte90@googlemail.com>
2025-04-15 08:42:35 +0200

    Add PokeREPL

--- main.go
+++ main.go
@@ -1,35 +1,25 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"pokedex/internal/pokecache"
-	"strings"
-	"time"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
 	myPokeDex := map[string]string{}
 	commMap = map[string]cliCommand{
 		"exit": {
-			name:        "exit",
+			name:        "exit\t",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
 		"help": {
-			name:        "help",
+			name:        "help\t",
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
 		"map": {
-			name:        "map",
+			name:        "map\t",
 			description: "Shows the next 20 areas",
 			callback:    commandMap,
 		},
 		"mapb": {
-			name:        "mapb",
+			name:        "mapb\t",
 			description: "Shows the previous 20 areas",
 			callback:    commandMapBack,
 		},
@@ -39,7 +29,7 @@ func main() {
 			callback:    commandExplore,
 		},
 		"catch": {
-			name:        "catch",
+			name:        "catch\t",
 			description: "Throw a pokeball at the pokemon you provide",
 			callback:    commandCatch,
 		},
@@ -49,47 +39,5 @@ func main() {
 			callback:    commandInspect,
 		},
 	}
-
-	cache := pokecache.NewCache(time.Duration(20 * time.Second))
-	next := "https://pokeapi.co/api/v2/location-area/"
-	conf := config{
-		Next:     &next,
-		Previous: nil,
-		Cache:    cache,
-		Pokedex:  &myPokeDex,
-	}
-	var args []string = nil
-	for {
-		args = []string{}
-		fmt.Print("Pokedex > ")
-		if !scanner.Scan() {
-			break
-		}
-		line := scanner.Text()
-		input := cleanInput(line)
-		if len(input) == 0 {
-			continue
-		}
-		if len(input) > 1 {
-			args = input[1:2]
-		}
-		command, ok := commMap[input[0]]
-		if !ok {
-			fmt.Println("Unknown command")
-			continue
-		}
-		command.callback(&conf, args...)
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "error reading input:", err)
-	}
-
-}
-
-func cleanInput(text string) []string {
-	words := strings.Fields(text)
-	for i, word := range words {
-		words[i] = strings.ToLower(word)
-	}
-	return words
+	PokeREPL(myPokeDex, commMap)
 }
---
+++ repl.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"pokedex/internal/pokecache"
+	"strings"
+	"time"
+)
+
+func PokeREPL(myPokeDex map[string]string, commands map[string]cliCommand) {
+	cache := pokecache.NewCache(time.Duration(20 * time.Second))
+	next := "https://pokeapi.co/api/v2/location-area/"
+	conf := config{
+		Next:     &next,
+		Previous: nil,
+		Cache:    cache,
+		Pokedex:  &myPokeDex,
+	}
+	var args []string = nil
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		args = []string{}
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		input := cleanInput(line)
+		if len(input) == 0 {
+			continue
+		}
+		if len(input) > 1 {
+			args = input[1:2]
+		}
+		command, ok := commands[input[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback(&conf, args...)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading input:", err)
+	}
+}
+
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+	for i, word := range words {
+		words[i] = strings.ToLower(word)
+	}
+	return words
+}
